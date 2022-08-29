5c202e10532c33ff0768d0df73bdc8ac7c2f4479
LuizSCijani <luiz.f.cijani>
2022-08-29 17:43:07 -0300

    .

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss', '../explore-container/explore-container.component.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page {
+  ladoA : string
+  ladoB : string
+  ladoC : string
+  resultado : string
+  mensagem : string
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'classificador',
+      subHeader: this.mensagem,
+      message: this.mensagem,
+      buttons: ['Fechar'],
+    });
+
+
+await alert.present();
+}
+
+  Calculo(){
+    var A = parseFloat(this.ladoA)
+    var B = parseFloat(this.ladoB)
+    var C = parseFloat(this.ladoC)
+    if((A+B)>C && (A+C)>B && (B+C)>A){
+      var p = (A + B + C) / 2
+
+      var Calculo = Math.sqrt(p * (p-A) * (p-B) * (p-C)) 
+
+      this.resultado = Calculo.toFixed(2)
+
+      this.presentAlert()
+    } 
+
+    else{
+      this.mensagem = "Tringulo não existe"
+    }
+
+    this.presentAlert()
+    
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss', '../explore-container/explore-container.component.scss']
 })
+
 export class Tab2Page {
+  ladoA : string
+  ladoB : string
+  ladoC : string
+  mensagem : string
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'classificador',
+        subHeader: this.mensagem,
+        message: this.mensagem,
+        buttons: ['Fechar'],
+      });
+
+
+  await alert.present();
+  }
+
+  Verificar(){
+    var A = parseFloat(this.ladoA)
+    var B = parseFloat(this.ladoB)
+    var C = parseFloat(this.ladoC)
+    
+    if(A + B > C && A + C > B && B + C > A){
+      this.classificador(A, B, C)
+    }
+
+  }
+
+  classificador(A, B, C){
+    if (A == B && A == C){
+      this.mensagem = "Tringulo Equilátero"
+      this.presentAlert()
+    }
+
+    else if (A != B && A != C && B != C){
+      this.mensagem = "Tringulo Escaleno"
+    }
+
+    else{
+      this.mensagem = "Tringulo Isósceles"
+    }                        
+      
+      this.presentAlert()
+  }
+          
 }
